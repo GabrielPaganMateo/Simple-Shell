d05e5b849ba27751e8c124cdf7d231ed83b914a0
AngelJCT <acarrion_pr@yahoo.com>
2022-11-29 11:43:52 +0000

    uploding main function file

--- simple_shell.c
+++ simple_shell.c
@@ -1,8 +1,22 @@
+#include "header.h"
 /**
- * Simple Shell
+ *main-principal function for simple shell
+ *Return: infinite loop to display prompt and read lines
  */
 int main(void)
 {
-	while (true)
+	while (1)
 	{
+		write(1, "$ ", 2);
 
+		char *line = read_line();
+		char **tokens = split_line(line);
+
+		if (tokens[0] != NULL)
+		{
+			execute(tokens);
+		}
+		free(tokens);
+		free(line);
+	}
+}
